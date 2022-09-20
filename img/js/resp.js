3a19a4bed05ee0525d0cfeb97cd95048e5d8f6e4
lopgi <113936021+lopgi@users.noreply.github.com>
2022-09-21 00:35:36 +0530

    webtask

---
+++ img/js/resp.js
@@ -0,0 +1,12 @@
+burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+navList = document.querySelector('.nav-List')
+rightNav = document.querySelector('.rightNav')
+burger.addEventListener('click', ()=>{
+    rightNav.classList.toggle('v-class-resp');
+    navList.classLList.toggle('v-class-resp');
+    navbar.classLList.toggle('h-nav-resp');
+
+
+
+})
